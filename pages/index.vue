d1cc5ee2bffd0ac8ae8f5c98d9448dcf26328f7b
New <new@MacBook-Pro-de-New.local>
2021-02-06 16:23:16 -0500

    first merge

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="main text-center flex flex-col items-center justify-center">
     <!-- <logo /> -->
     <h1 class="title">Nuxt — Tailwind — Netlify CMS</h1>
-    <h2 class="subtitle">Boilerplate</h2>
+    <h2 class="subtitle">This site will become an agency portfolio template</h2>
   </div>
 </template>
 
